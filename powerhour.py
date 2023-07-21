c052796a89213094e0d5752c79f10f4167de1f8b
clayton3 <claytoncblevins@gmail.com>
2023-07-21 17:18:44 -0400

    Ironed out bugs

--- powerhour.py
+++ powerhour.py
@@ -3,6 +3,9 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 import playsound
+import pyautogui
+import math
+
 
 #Main Driver function
 def main():
@@ -24,7 +27,7 @@ def setup(url):
     browser = webdriver.Chrome(options=options)
     browser.get(url)
 
-    time.sleep(5)
+    time.sleep(2)
 
     browser.refresh()
 
@@ -32,6 +35,12 @@ def setup(url):
 
 #Function to drive power hour
 def powerHour(browser):
+    closeCaption = browser.find_elements(By.CSS_SELECTOR, '.ytp-subtitles-button.ytp-button')
+    if(closeCaption):
+        closeCaption[0].click()
+    else:
+        print("Close Caption not available")
+
     fullscreen = browser.find_elements(By.CSS_SELECTOR, '.ytp-fullscreen-button.ytp-button')
     if(fullscreen):
         fullscreen[0].click()
@@ -42,11 +51,21 @@ def powerHour(browser):
     if(skip):
         playGame = True
         while(playGame):
+            if(browser.find_elements(By.CSS_SELECTOR, "div.reason.style-scope.ytd-player-error-message-renderer")):
+                skip[0].click()
+
             playsound.playsound("sounds/beer.wav")
             browser.execute_script('document.getElementsByTagName("video")[0].currentTime += 60;')
             time.sleep(60)
             skip[0].click()
+            move()
     else:
         print("Skip button not found")
 
-#main()
+def move():
+    screen_width, screen_height = pyautogui.size()
+    target_x = 0
+    target_y = screen_height // 2
+    pyautogui.moveTo(target_x, target_y)
+
+main()
